585b5d332ec4f434f11d31e6a063e370f37fae1d
Gonzalo Berhouet <gonzalo.berhouet@ecs.co.uk>
2022-09-28 17:37:49 +0100

    django model

--- backend/resthome/settings.py
+++ backend/resthome/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f#z#(m+z27kf-u@808t7r!9v!or0e@3wga-&s^6n+t=qn6z#0s'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'products'
 ]
 
 MIDDLEWARE = [
