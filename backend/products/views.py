ac140d2877225cc31895b922e0a2426c74166b73
Gonzalo Berhouet <gonzalo.berhouet@ecs.co.uk>
2022-10-11 11:28:31 +0100

    permissions added

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,15 +1,18 @@
-from re import A
-from rest_framework import generics, mixins
+from rest_framework import authentication, generics, mixins, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Product
+from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
@@ -27,7 +30,6 @@ product_list_create_view = ProductListCreateAPIView.as_view()
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
 
     # lookupfield='pk'
 product_detail_view = ProductDetailAPIView.as_view()
